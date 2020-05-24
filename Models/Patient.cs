7e0980b64972a1eb7ae0b50900dbdae20426ea88
s18938 <s18938@pjwstk.edu.pl>
2020-05-24 11:31:08 +0200

    Add project files.

---
+++ Models/Patient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD11.Models
+{
+    public class Patient
+    {
+        public Patient()
+        {
+            Prescription = new HashSet<Prescription>();
+        }
+
+        public int IdPatient { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthdate { get; set; }
+
+        public virtual ICollection<Prescription> Prescription { get; set; }
+    }
+}
---
+++ Models/PrescriptionMedicament.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APBD11.Models
+{
+    public class PrescriptionMedicament
+    {
+        public int IdMedicament { get; set; }
+        public int IdPrescription { get; set; }
+        public int Dose { get; set; }
+        public string Details { get; set; }
+
+        public virtual Medicament IdMedicamentNavigation { get; set; }
+        public virtual Prescription IdPrescriptionNavigation { get; set; }
+    }
+}
